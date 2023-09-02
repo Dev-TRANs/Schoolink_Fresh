2138ae85d4f44c5b8d2502b15166217ca81d17de
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-09-02 12:31:23 +0900

    Update AdminPage.tsx

--- islands/admin/AdminPage.tsx
+++ islands/admin/AdminPage.tsx
@@ -5,21 +5,23 @@ import CreateUser from './components/CreateUser.tsx'
 
 export default () => {
   const [password, setPassword] = useState('')
-  return <div>
-    <div>Schoolink Admin Page</div>
+  return <div class='mx-5'>
+    <div class='text-3xl'>Schoolink Admin Panel</div>
+    <p>SchoolinkのAdmin用パネルです。操作にはパスワードが必要です。</p>
     <div>
-      <div>
+      <div class='my-4'>
         <label>Password:</label>
         <input type='password' onInput={(evt) => {
           setPassword(evt.target.value)
         }} value={password} />
       </div>
+      <p></p>
     </div>
     <div class='flex-col gap-4'>
-      <div class='border p-2 rounded'>
+      <div class='border p-2 rounded my-2 bg-white drop-shadow-md'>
         <CreateSchool password={password} />
       </div>
-      <div class='border p-2 rounded'>
+      <div class='border p-2 rounded my-2 bg-white drop-shadow-md'>
         <CreateUser password={password} />
       </div>
     </div>
