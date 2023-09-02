e6ce0ab8b1b8142f7d90122d290751295baaff3d
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-09-02 13:03:17 +0900

    Update CreateSchool.tsx

--- islands/admin/components/CreateSchool.tsx
+++ islands/admin/components/CreateSchool.tsx
@@ -1,10 +1,14 @@
 import { useState } from 'preact/hooks'
+import ky from 'ky'
 
 export interface Props {
   password: string
 }
 
 export default (props: Props) => {
+  const [schoolName, setSchoolName] = useState('')
+  const [createdSchoolId, setCreatedSchoolId] = useState('')
+  
   return <div>
     <div>
       <div class="text-2xl">Create School</div>
@@ -16,10 +20,36 @@ export default (props: Props) => {
         <div>
           <div>
             <label>正式名称</label>
-            <input className='border' placeholder='正式名称' />
+            <input className='border' placeholder='正式名称' value={schoolName} onInput={e => setSchoolName(e.target.value)}/>
           </div>
         </div>
-        <div><button>学校を作成する</button></div>
+        <div>
+          <button class='border bg-gray-200' onClick={async () => {
+            if (!confirm(`${schoolName}という学校を作成しますか？`)) {
+              return
+            }
+            let result
+            try {
+              result = await ky.post('/admin/api', {
+                json: {
+                  type: 'create_school',
+                  schoolName: schoolName,
+                }
+              })
+            } catch (_error) {
+              alert('エラーが発生しました。パスワードが間違っている可能性があります。')
+            }
+            setCreatedSchoolId(result.schoolId)
+          }}>学校を作成する</button>
+        </div>
+        {
+          createdSchoolId &&<div>
+            <div>{ schoolName }の作成に成功しました！</div>
+            <div>
+              <div>IDは、 {createdSchoolId}です。</div>
+            </div>
+          </div>
+        }
       </div>
     </div>
   </div>
