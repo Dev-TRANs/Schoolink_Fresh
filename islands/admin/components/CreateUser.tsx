0c6716e09c896ff81b2c1742b4c30337c516643e
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-09-02 12:19:06 +0900

    Update CreateUser.tsx

--- islands/admin/components/CreateUser.tsx
+++ islands/admin/components/CreateUser.tsx
@@ -1,7 +1,37 @@
 import { useState } from 'preact/hooks'
 
+export interface Props {
+  password: string
+}
+
 export default (props: Props) => {
   return <div>
-    <div></div>
+    <div>
+      <div class="text-2xl">Create User</div>
+      <p>ユーザーを作成する</p>
+    </div>
+    <div>
+      <div>
+        <div>新しいユーザーについて</div>
+        <div>
+          <div>
+            <label>ID</label>
+            <input className='border' placeholder='新しいユーザーID' />
+          </div>
+          <div>
+            <label>Name</label>
+            <input className='border' placeholder='表示名' />
+          </div>
+          <div>
+            <label>School ID</label>
+            <input className='border p-2' placeholder='School ID' />
+          </div>
+          <div>
+            <label>連絡先</label>
+            <input className='border p-2' placeholder='連絡先' />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 }
