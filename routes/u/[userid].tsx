1b45e55bd04798059fea8b0a6a271c998a2ea531
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-10-01 14:39:28 +0900

    Create [userid].tsx

---
+++ routes/u/[userid].tsx
@@ -0,0 +1,10 @@
+// ユーザーのプロフィール
+
+export default function UserProfile() {
+  return (
+    <div>
+      <div class='text-2xl'>Schoolink</div>
+      <div class=''>By TRANS</div>
+    </div>
+  )
+}
