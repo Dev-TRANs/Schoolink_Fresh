ce53c7d60998b78a0bfb1d0d5f56f205f0c562f9
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-10-01 14:41:53 +0900

    Create Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header () {
+  return <header class="sticky t-0 w-full">
+    <div class="flex justify-between">
+      <a href="/">
+        <div>
+          Schoolink
+        </div>
+      </a>
+      <div>
+        
+      </div>
+    </div>
+  </header>
+}
